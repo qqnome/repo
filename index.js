450b2d259a2ad6ada9bc62436e809319557ad918
Mou <mou_orange@163.com>
2019-04-01 14:44:45 +0800

    to branch bak

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-function out(){
-    console.log('out')
-}
+function query2json(url) {
 
-export default out;
+    let hashIndex = url.indexOf('#'),
+        strQuery,
+        arrQuery,
+        map = {};
+
+    strQuery = url.substring(url.indexOf('?') + 1, hashIndex < 0 ? undefined : hashIndex);
+    arrQuery = strQuery.split('&');
+    for (let i = 0, len = arrQuery.length; i < len; i++) {
+        let queryItem = arrQuery[i],
+            arrTemp = queryItem.split('='),
+            key = arrTemp[0];
+        if (arrTemp.length > 0 && key) {
+            map[key] = arrTemp[1] || '';
+        }
+    }
+
+    return map;
+};
+
+exports.query2json = query2json;
